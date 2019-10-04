0e0ba94f6f9cceee3bac1178f545e989a986f0a6
FCraven <craven.francis@gmail.com>
2019-10-04 14:50:06 -0400

    styling CurrentWeatherComponent

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -6,7 +6,24 @@ const CurrentWeather = (props) => {
   const { current, place, time } = props;
   const temperature = Math.round(current.temperature)
   const feelsLike = Math.round(current.apparentTemperature)
+
+  const navBtnObj = {
+    minute: {
+
+    },
+    hour: {
+
+    },
+    day: {
+
+    },
+    week: {
+
+    }
+  }
+
   console.log(`PROPS -->`, props)
+
 
   return (
     <section id='current-weather-container'>
@@ -16,9 +33,12 @@ const CurrentWeather = (props) => {
       <div id='current-weather-sunny'></div>
       <div id='current-weather-water'>
         <div id="current-weather-sand">
-          <div id='current-weather-info-container'>
-            
-          </div>
+          <nav id='current-weather-info-container'>
+            <btn class='current-weather-nav-btn'>:01</btn>
+            <btn class='current-weather-nav-btn'>:60</btn>
+            <btn class='current-weather-nav-btn'>24</btn>
+            <btn class='current-weather-nav-btn'>7</btn>
+          </nav>
         </div>
       </div>
 
